44927d691c3f7fa6dde178bbd4bae3836ca7d977
barbielm <barbosagabriel0508@gmail.com>
2021-06-22 11:30:22 -0300

    CRUD games

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pg from 'pg';
 import Joi from 'joi';
+import checkIfCategoryExists from './checkIfCategoryExists.js';
+
 
 const server = express();
 server.use(cors());
@@ -21,11 +23,26 @@ const categoriesSchema = Joi.object({
     name: Joi.string().required()
 })
 
+const gamesSchema = Joi.object({
+    name: Joi.string().required(),
+    image: Joi.string().pattern(new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?' + // fragment locator
+    '(\\.(jpe?g|png|gif|bmp))$' // extensions
+    ,'i')), 
+    stockTotal: Joi.number().integer().min(1).required(),
+    categoryId: Joi.number().integer().required(),
+    pricePerDay: Joi.number().integer().min(1).required()
+
+})
+
 
 server.get('/categories', async (req,res) => {
     const categories = await connection.query('SELECT * FROM categories');
     res.send(categories.rows);
-    console.log(categories.rows);
 })
 
 server.post('/categories', async (req,res) => {
@@ -51,6 +68,41 @@ server.post('/categories', async (req,res) => {
         res.sendStatus(400);
     }
     
+})
+
+server.get('/games', async (req,res) => {
+    if(!req.query.name){
+        const games = await connection.query(`SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id`);
+        res.send(games.rows);
+    } else {
+        const games = await connection.query(`SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id WHERE LOWER(games.name) LIKE $1`,[req.query.name + '%']);
+        res.send(games.rows);
+    }
+})
+
+server.post('/games', async (req,res) => {
+    const {error} = gamesSchema.validate(req.body);
+    if(!error){
+        const gamesCategories = await connection.query("SELECT * FROM categories");
+        if(checkIfCategoryExists(req.body, gamesCategories.rows)){
+            const {name,image,stockTotal,categoryId,pricePerDay} = req.body;
+            const gamesNames = await connection.query("SELECT * FROM games");
+            if(gamesNames.rows.length !== 0){
+                for(let i = 0; i < gamesNames.rows.length; i++){
+                    if(gamesNames.rows[i].name === req.body.name){
+                        res.sendStatus(409);
+                        return;
+                    }
+                }
+            }
+            const insertNewGame = await connection.query(`INSERT INTO games(name,image,"stockTotal","categoryId","pricePerDay") VALUES ($1,$2,$3,$4,$5)`,[name,image,stockTotal,categoryId,pricePerDay])
+            res.sendStatus(201);
+            return;
+        }
+        res.sendStatus(400);
+        return;
+    }
+    res.sendStatus(400);
 })
 
 server.listen(4000);
